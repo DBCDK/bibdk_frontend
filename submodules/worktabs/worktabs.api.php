2a420ef77cc2d9c33eb03a1e16a7dc772c6f7624
Martin Møller <mmj@dbc.dk>
2013-04-17 12:33:24 +0200

    API file added

---
+++ submodules/worktabs/worktabs.api.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file API documentation of hook_worktabs_items().
+ */
+
+
+/**
+ * Hook used by the worktabs module to retrieve content from other modules,
+ * implementing the hook, to display embedded in the tabs.
+ *
+ *  'title':  String giving the title of the current item. If an empty string is
+ *            provided no title will be set.
+ *
+ *  'btn_txt_closed:' Optional string, setting the title of the open/close
+ *                    button when the item untoggled.
+ *
+ *  'btn_txt_open': Optional string, setting the title of the open/close button
+ *                  when the item is toggled.
+ *
+ *  'rendered': String containing ready for disaplay rendered HTML.
+ *
+ *  'weight': Integer indicating the weight of the item.
+ *            Lower number = rendered as first item.
+ *
+ *
+ * @param array $tabs Array describing the maintabs
+ * @param bibdkWork $entity
+ *
+ * @return array
+ *
+ * @see worktabs.module - worktabs_render_content($tabs)
+ *
+ */
+function hook_worktabs_items($tabs, $entity) {
+  $tabs['PARENT_TAB']['MY_ITEM'] = array(
+    'title' => '',
+    'btn_txt_closed' => '',
+    'btn_txt_open' => '',
+    'rendered' => '',
+    'weight' => 0,
+    );
+  return $tabs;
+}
