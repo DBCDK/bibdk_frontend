21429ac46f8173a39486bafc5365c7a9dcd325e1
Martin Møller <mmj@dbc.dk>
2013-10-09 13:01:52 +0200

    Codeformatting

--- features/bibdk_article/theme/bibdk-articles-block-widget.tpl.php
+++ features/bibdk_article/theme/bibdk-articles-block-widget.tpl.php
@@ -1,28 +1,28 @@
 <article <?php print $class; ?> >
-	  <div class="inner">
-				<div class="field-title">
-					<h3><a href="<?php print $link; ?>"><?php print $title; ?></a></h3>
-				</div>
-      <?php if (!empty($image)) : ?>
+  <div class="inner">
+    <div class="field-title">
+      <h3><a href="<?php print $link; ?>"><?php print $title; ?></a></h3>
+    </div>
+    <?php if (!empty($image)) : ?>
       <div class="field-image">
-				  <?php print $image; ?>
-				</div>
-			<?php endif; ?>
-			<?php if (!empty($body)): ?>
-				<div class="field-body">
-				  <?php print $body; ?>
-				</div>
-			<?php endif; ?>
-      <?php if (!empty($link)): ?>
-			<footer class="footer">
-					<div class="field-read-more">
-            <?php print $link; ?>
-            <span class="icon icon-left icon-blue-right">&nbsp;</span>
-					</div>
-			</footer>
-      <?php endif; ?>
-		</div>
-		<!-- inner -->
+        <?php print $image; ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($body)): ?>
+      <div class="field-body">
+        <?php print $body; ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($link)): ?>
+      <footer class="footer">
+        <div class="field-read-more">
+          <?php print $link; ?>
+          <span class="icon icon-left icon-blue-right">&nbsp;</span>
+        </div>
+      </footer>
+    <?php endif; ?>
+  </div>
+  <!-- inner -->
 
   <!-- widget -->
 
