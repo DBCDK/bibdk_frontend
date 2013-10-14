abbed7e5ba543648c8caeeb1fecdbc2bf12cb1c8
Anders-Andersen <ana@dringo.dbc.dk>
2013-10-14 09:57:48 +0200

    codereview tpl file  deleted

--- features/bibdk_article/theme/bibdk-articles-block.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php foreach($articles as $article_row) : ?>
-    <div class="<?php echo $article_row['width']; ?>">
-      <?php foreach($article_row['articles'] as $article) : ?>
-        <?php print $article; ?>
-      <?php endforeach;?>
-        </div>
-  <?php endforeach;?>
