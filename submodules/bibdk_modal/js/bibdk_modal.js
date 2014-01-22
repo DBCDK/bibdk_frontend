005135b68a11b3eadb0fee850edc77a0ff7c7766
Martin Møller <mmj@dbc.dk>
2014-01-22 10:59:36 +0100

    Minor

--- submodules/bibdk_modal/js/bibdk_modal.js
+++ submodules/bibdk_modal/js/bibdk_modal.js
@@ -1,15 +1,15 @@
 (function($) {
 
-   //TODO set links that should be rewritten in a settings object
+  //TODO set links that should be rewritten in a settings object
   Drupal.behaviors.bibdk_modal = {
     attach: function(context, settings) {
 
       //Rewrite login link
-      $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-modal-forms-login', function () {
-        this.href = this.href.replace(/user\/login/,'bibdk_modal/nojs/login');
+      $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-modal-forms-login',function() {
+        this.href = this.href.replace(/user\/login/, 'bibdk_modal/nojs/login');
       }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
     }
-  }
+  };
 })(jQuery);
 
 
