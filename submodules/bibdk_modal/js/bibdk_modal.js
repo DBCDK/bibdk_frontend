7d66a8740c8684896bcf649068a49de9c7ed2643
Martin Møller <mmj@dbc.dk>
2014-01-29 10:27:01 +0100

    Cleanup and better handling of reload of current path when login is successful

--- submodules/bibdk_modal/js/bibdk_modal.js
+++ submodules/bibdk_modal/js/bibdk_modal.js
@@ -1,22 +1,28 @@
 (function($) {
 
-  //TODO set links that should be rewritten in a settings object
+  var BibdkModal = {};
+
+  BibdkModal.setLinkActions = function(context){
+    //Rewrite login link
+    $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-modal-forms-login',function() {
+      this.href = this.href.replace(/user\/login/, 'bibdk_modal/nojs/login');
+    }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
+
+    //Rewrite forgot password link
+    $("a[href*='/user/password'], a[href*='?q=user/password']", context).once('init-modal-forms-login',function() {
+      this.href = this.href.replace(/user\/password/, 'bibdk_modal/nojs/password');
+    }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
+
+    //Rewrite register new user link
+    $("a[href*='/user/register'], a[href*='?q=user/register']", context).once('init-modal-forms-login',function() {
+      this.href = this.href.replace(/user\/register/, 'bibdk_modal/nojs/register');
+    }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
+  };
+
   Drupal.behaviors.bibdk_modal = {
     attach: function(context, settings) {
-      //Rewrite login link
-      $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-modal-forms-login',function() {
-        this.href = this.href.replace(/user\/login/, 'bibdk_modal/nojs/login');
-      }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
 
-      //Rewrite forgot password link
-      $("a[href*='/user/password'], a[href*='?q=user/password']", context).once('init-modal-forms-login',function() {
-        this.href = this.href.replace(/user\/password/, 'bibdk_modal/nojs/password');
-      }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
-
-      //Rewrite register new user link
-      $("a[href*='/user/register'], a[href*='?q=user/register']", context).once('init-modal-forms-login',function() {
-        this.href = this.href.replace(/user\/register/, 'bibdk_modal/nojs/register');
-      }).addClass('ctools-use-modal  ctools-modal-bibdk-modal-style');
+      BibdkModal.setLinkActions(context);
     }
   };
 })(jQuery);
