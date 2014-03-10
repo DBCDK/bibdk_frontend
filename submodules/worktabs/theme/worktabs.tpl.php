4ce710b54e70717affcc613271ce8666f60ac7cb
Michael Carlsson <mca@dbc.dk>
2014-03-10 13:57:27 +0100

    Template updated to use render arrays and add a tabs-sections-wrapper.

--- submodules/worktabs/theme/worktabs.tpl.php
+++ submodules/worktabs/theme/worktabs.tpl.php
@@ -3,19 +3,21 @@
   <!-- tabs-nav -->
   <div class="tabs-nav clearfix">
     <?php foreach ($tabs as $type => $tab) : ?>
-      <a href="#<?php print $type . $pid; ?>" id='<?php print $type; ?>' class="<?php print $type; ?> <?php print $tab['class']; ?>"><?php print $tab['title']; ?></a>
+    <a href="#<?php print $type . $pid; ?>" id='<?php print drupal_html_id($type); ?>' class="<?php print $type; ?> <?php print $tab['class']; ?>"><?php print render($tab['title']); ?></a>
     <?php endforeach; ?>
   </div>
   <!-- tabs-nav -->
   <div class="tabs-sections">
-    <?php foreach ($tabs as $type => $tab) : ?>
-      <div id="<?php print $type . $pid; ?>" class="tabs-section <?php print $tab['active']; ?>">
-        <div class="text clearfix">
-          <?php print $tab['content']; ?>
+    <div class="tabs-sections-wrapper">
+      <?php foreach ($tabs as $type => $tab) : ?>
+        <div id="<?php print $type . $pid; ?>" class="tabs-section <?php print $tab['active']; ?>">
+          <div class="text clearfix">
+            <?php print render($tab['content']); ?>
+          </div>
         </div>
-      </div>
-    <?php endforeach; ?>
-    <!-- tabs-section -->
+      <?php endforeach; ?>
+      <!-- tabs-section -->
+    </div>
   </div>
   <!-- tabs-sections -->
 </div>
