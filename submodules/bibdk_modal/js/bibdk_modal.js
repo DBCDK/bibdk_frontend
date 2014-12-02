09a5b4d3711388e138a2a398fe13c39906b794a9
Martin Møller <mmj@dbc.dk>
2014-12-02 11:28:35 +0100

    cleanup

--- submodules/bibdk_modal/js/bibdk_modal.js
+++ submodules/bibdk_modal/js/bibdk_modal.js
@@ -5,7 +5,6 @@
   // ===========================================================================
   //   COMMANDS
   // ---------------------------------------------------------------------------
-
   Drupal.ajax.prototype.commands.bibdk_modal_reload = function(ajax, data, status) {
     BibdkModal.detachBehaviorsOnModal();
     var currentUrl = document.URL;
@@ -43,7 +42,6 @@
   Drupal.ajax.prototype.commands.bibdk_modal_redirect = function(ajax, data, status) {
     location.href = data.url;
   };
-
   // ---------------------------------------------------------------------------
   //   COMMANDS -- END
   // ===========================================================================
@@ -142,90 +140,15 @@
     $modal.removeAttr("role");
   };
 
-  BibdkModal.bindEvents = function(context) {
-    $('#modalBackdrop', context).bind('click', BibdkModal.closeModal);
-    $('.close', context).bind('click', BibdkModal.setMainwrapperAttr);
-    $(document).bind('keydown', BibdkModal.keyEventHandler);
-    $('body', context).bind('focus', BibdkModal.modalHasFocus);
-  };
-
-  BibdkModal.unbindEvents = function() {
-    $('#modalBackdrop').unbind('click', BibdkModal.closeModal);
-    $(document).unbind('keydown', BibdkModal.keyEventHandler);
-    $('body').unbind('focus', BibdkModal.modalHasFocus);
-  };
-
-  BibdkModal.keyEventHandler = function(event) {
-    if(event.keyCode == 27) {
-      BibdkModal.closeModal();
-      return false;
-    }
-  };
-
-  BibdkModal.closeModal = function() {
-    $('.close').trigger('click');
-    BibdkModal.unbindEvents();
-  };
-
   BibdkModal.setMainwrapperAttr = function() {
     $('#mainwrapper').attr("aria-hidden", false);
-  };
-
-  /**
-   * Set the modal window 10% from top of current viewport
-   */
-  BibdkModal.modalHasFocus = function() {
-    jQuery('.exit-off-canvas').trigger('click');
-    var top = (window.innerHeight * 0.1) + $(window).scrollTop();
-    $('#modalContent').css({top: top + 'px'});
-  };
-
-  /**
-   * Overrides Drupal.CTools.Modal.modal_display in ctools/js/modal.js to add
-   * autofocus on name input field.
-   */
-  BibdkModal.overrideCtoolsModalDisplay = function() {
-    Drupal.CTools.Modal.modal_display = function(ajax, response, status) {
-      if($('#modalContent').length == 0) {
-        Drupal.CTools.Modal.show(Drupal.CTools.Modal.getSettings(ajax.element));
-      }
-      $('#modal-title').html(response.title);
-      // Simulate an actual page load by scrolling to the top after adding the
-      // content. This is helpful for allowing users to see error messages at the
-      // top of a form, etc.
-      $('#modal-content').html(response.output).scrollTop(0);
-      Drupal.attachBehaviors();
-      $('#edit-name').focus();
-    };
-  };
-
-  BibdkModal.attachAjax = function(modal) {
-    console.log('Is this in use? --> BibdkModal.attachAjax');
-    //TODO mmj probably unused
-    console.log(modal);
-    var $form = $('.element-wrapper', modal);
-    console.log($form);
-    console.log('attached');
-    var base = $(modal).attr('id');
-    console.log(base, 'base');
-
-    console.log(base);
-    console.log(this);
-
-    var element_settings = {
-      url: window.location.origin + Drupal.settings.basePath + Drupal.settings.pathPrefix + 'system/ajax'
-    };
-
-    Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
-    $(this).click();
   };
 
   Drupal.behaviors.bibdk_modal = {
     attach: function(context, settings) {
       BibdkModal.setLinkActions(context);
-      BibdkModal.bindEvents(context);
-      BibdkModal.overrideCtoolsModalDisplay();
     },
+
     detach: function(context) {
       BibdkModal.removeAccessibilityInfo(context);
     }
