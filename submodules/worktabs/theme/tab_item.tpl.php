febf6835fefeed198d09010a28117f4f26d9ab7d
Søren Vibjerg <vibjerg@gmail.com>
2015-01-13 11:27:18 +0100

    Do not show toggle buttons if only a single element exists

--- submodules/worktabs/theme/tab_item.tpl.php
+++ submodules/worktabs/theme/tab_item.tpl.php
@@ -4,11 +4,13 @@
     <?php print render($title); ?>
   </span>
 
+  <?php if (!$single) : ?>
   <a href="#"  <?php !empty($more_id) ? print 'id="'.$more_id.'"' : '';?>" class="tab-link toggle-worktab-content text-small<?php ($single) ? print ' toggled' : print ''; ?>">
     <span class="icon icon-left icon-blue-down">&nbsp;</span>
-    <span class="toggle-text<?php ($single) ? print ' hidden' : print ''; ?>"><?php print render($btn_txt_closed); ?></span>
-    <span class="toggle-text <?php (!$single) ? print ' hidden' : print ''; ?>"><?php print render($btn_txt_open); ?></span>
+    <span class="toggle-text"><?php print render($btn_txt_closed); ?></span>
+    <span class="toggle-text hidden"><?php print render($btn_txt_open); ?></span>
   </a>
+  <?php endif; ?>
 
   <div class="tab-content <?php (!$single) ? print 'visuallyhidden' : print ''; ?>">
     <?php print render($content); ?>
