0e57b4cea2f47f6d4d5f2e77a99a3d6a31442b45
Søren Vibjerg <vibjerg@gmail.com>
2015-01-19 09:58:24 +0100

    remove unused code + reactivate toggle text on reviews tab

--- submodules/worktabs/js/worktabs.js
+++ submodules/worktabs/js/worktabs.js
@@ -1,27 +1,4 @@
 (function($) {
-  function itemClicked(element) {
-    hideItems(element);
-    showItem(element);
-  }
-
-  function hideItems(items) {
-    $(items).parent().siblings('.tab-item').each(function(index, value) {
-      if($(value).children('.tab-title').hasClass('active')) {
-        $(value).children('.tab-title').toggleClass('active');
-        $(value).children('.tab-content').toggleClass('visuallyhidden');
-        $(value).children('.toggle-text').toggleClass('hidden');
-        $(value).children('.tab-link').children('.toggle-text').toggleClass('hidden');
-      }
-    });
-  }
-
-  function showItem(item) {
-    item.toggleClass('toggled');
-    item.parent().children('.tab-title').toggleClass('active');
-    item.next('.tab-content').toggleClass('visuallyhidden');
-    item.children('.toggle-text').toggleClass('hidden');
-  }
-
   function worktabsClick(item) {
     var id = item[0].getAttribute('href');
     $(id + ' .tab-content').bind('DOMNodeInserted', function() {
@@ -40,15 +17,10 @@
 
   Drupal.behaviors.worktabs = {
     attach: function(context) {
-      /*$('.tab-link', context).once().click(function(e) {
-        e.preventDefault();
-        itemClicked($(this));
-      });
-    */
-      /*$('.worktabs .tabs-nav a', context).once().click(function(e) {
+      $('.worktabs .tabs-nav a', context).once().click(function(e) {
         e.preventDefault();
         worktabsClick($(this));
-      });*/
+      });
     }
   };
 }(jQuery));
